4128da19f6cf2660a2067f814b92218d328bc5cb
MrMusic64 <ethan08anderson@gmail.com>
2025-07-14 21:05:59 -0500

    Added more to the choosers

--- src/main/java/frc/robot/current/subsystems/LedOperation.java
+++ src/main/java/frc/robot/current/subsystems/LedOperation.java
@@ -11,19 +11,20 @@ import frc.robot.lib.leds.LedColor;
 import frc.robot.lib.leds.LedController;
 
 public class LedOperation extends SubsystemBase {
-  public static final LedController leds = new LedController(300, 3, .75);
+  public static final LedController leds = new LedController(200, 3, .75);
 
   // Constants regarding manual LED states
   private static final String setSolid = "Solid";
   private static final String setTwoToneSolid = "Two Color Orange and Magenta";
   private static final String rainbow = "Rainbow";
-  private static final String waveBlueGreen = "Wave Blue and Green";
-  private static final String breathLightBlue = "Breath LightBlue";
-  private static final String strobeRed = "Red Strobe";
+  private static final String fadeBlueGreen = "Wave Blue and Green";
+  private static final String breathColor = "Breath";
+  private static final String strobeColor = "Strobe";
+  private static final String carnivalEasterGreenPurple = "Carnival";
   private static final String setColorBlack = "Black Light";
-  private static final String fillGreen = "Fill pink";
-  private static final String zipAqua = "Zip Aqua";
-  private static final String wavePurple = "Wave Purple";
+  private static final String fillColor = "Fill";
+  private static final String zipColor = "Zip";
+  private static final String waveColor = "Wave";
   private static final String colorTest = "Color Testing";
   private String manualLedState;
 
@@ -36,10 +37,19 @@ public class LedOperation extends SubsystemBase {
   private static final String lime = "lime";
   private static final String green = "green";
   private static final String aqua = "aqua";
+  private static final String seaBlue = "sea blue";
   private static final String lightBlue = "light Blue";
+  private static final String skyBlue = "Sky Blue";
   private static final String blue = "Blue";
   private static final String cornflower = "Cornflower";
   private static final String indigo = "indigo";
+  private static final String lightPurple = "Light Purple";
+  private static final String purple = "Purple";
+  private static final String lightPink = "Light Pink";
+  private static final String rose = "rose";
+  private static final String magenta = "Magenta";
+  private static final String brown = "Brown";
+  private static final String white = "White";
   private String ledColor;
   private LedColor color;
   private final SendableChooser<String> m_chooser = new SendableChooser<>();
@@ -53,36 +63,53 @@ public class LedOperation extends SubsystemBase {
   public boolean automaticLED = false;
 
   public LedOperation() {
-    leds.addSection("full", 0, 58);
-    leds.addSection("front", 0, 27);
-    leds.addSection("back", 27, 58);
-    leds.addSection("top", 19, 34);
-    leds.addSection("frontBottom", 0, 19);
-    leds.addSection("backBottom", 34, 58);
+    leds.addSection("full", 0, 200);
+    leds.addSection("right", 41, 80);
+    leds.addSection("top", 81, 120);
+    leds.addSection("left", 121, 160);
+    leds.addSection("underglow", 0, 40);
+    leds.addSection("underglow", 161, 200);
 
     m_chooser.setDefaultOption("Solid", setSolid);
     m_chooser.addOption("Two Color Orange and Magenta", setTwoToneSolid);
     m_chooser.addOption("Solid Black", setColorBlack);
     m_chooser.addOption("Rainbow", rainbow);
-    m_chooser.addOption("Wave Blue and Green", waveBlueGreen);
-    m_chooser.addOption("Breath Light Blue", breathLightBlue);
-    m_chooser.addOption("Red Strobe", strobeRed);
-    m_chooser.addOption("Fill Green", fillGreen);
-    m_chooser.addOption("Zip Aqua", zipAqua);
-    m_chooser.addOption("Wave Purple", wavePurple);
+    m_chooser.addOption("Wave Blue and Green", fadeBlueGreen);
+    m_chooser.addOption("Breath Light Blue", breathColor);
+    m_chooser.addOption("Red Strobe", strobeColor);
+    m_chooser.addOption("Carnival", carnivalEasterGreenPurple);
+    m_chooser.addOption("Fill Green", fillColor);
+    m_chooser.addOption("Zip Aqua", zipColor);
+    m_chooser.addOption("Wave Purple", waveColor);
     m_chooser.addOption("Color testing", colorTest);
 
     m_color.addOption("Red", red);
-    m_color.addOption("", orange);
-    m_color.addOption("", yellow);
-    m_color.addOption("", yellowGreen);
-    m_color.addOption("", green);
-    m_color.addOption("", lightBlue);
-    m_color.addOption("", blue);
-    m_color.addOption("", cornflower);
+    m_color.addOption("Red-Orange", redOrange);
+    m_color.addOption("Orange", orange);
+    m_color.addOption("Gold", gold);
+    m_color.addOption("Yellow", yellow);
+    m_color.addOption("Yellow-Green", yellowGreen);
+    m_color.addOption("Lime", lime);
+    m_color.addOption("Green", green);
+    m_color.addOption("Aqua", aqua);
+    m_color.addOption("Sea Blue", seaBlue);
+    m_color.addOption("Light Blue", lightBlue);
+    m_color.addOption("Sky Blue", skyBlue);
+    m_color.addOption("Blue", blue);
+    m_color.addOption("Cornflower", cornflower);
+    m_color.addOption("Indigo", indigo);
+    m_color.addOption("Light Purple", lightPurple);
+    m_color.addOption("purple", purple);
+    m_color.addOption("Light Pink", lightPink);
+    m_color.addOption("Rose", rose);
+    m_color.addOption("Magenta", magenta);
+    m_color.addOption("Brown", brown);
+    m_color.addOption("White", white);
+
     SmartDashboard.putData("Manual LED", m_chooser);
     SmartDashboard.putData("LEDColor", m_color);
     manualLedState = m_chooser.getSelected();
+    ledColor = m_color.getSelected();
   }
 
   @Override
@@ -120,27 +147,27 @@ public class LedOperation extends SubsystemBase {
 
   public void updateState() {
     // switch (supersystemState.getMode()) {
-    //   case CORAL:
-    //     leds.solid("top", LedColor.BRIGHT_PURPLE);
-    //     break;
-    //   case ALGAE:
-    //     leds.solid("top", LedColor.AQUA);
+    // case CORAL:
+    // leds.solid("top", LedColor.BRIGHT_PURPLE);
+    // break;
+    // case ALGAE:
+    // leds.solid("top", LedColor.AQUA);
     // }
 
     // if (supersystemState.isElevatorGoingUp()) {
-    //   leds.fill("frontBottom", LedColor.YELLOW, 1, 3, false);
-    //   leds.fill("backBottom", LedColor.YELLOW, 1, 3, true);
+    // leds.fill("frontBottom", LedColor.YELLOW, 1, 3, false);
+    // leds.fill("backBottom", LedColor.YELLOW, 1, 3, true);
     // } else if (supersystemState.isElevatorGoingDown()) {
-    //   leds.fill("frontBottom", LedColor.YELLOW, 1, 3, true);
-    //   leds.fill("backBottom", LedColor.YELLOW, 1, 3, false);
+    // leds.fill("frontBottom", LedColor.YELLOW, 1, 3, true);
+    // leds.fill("backBottom", LedColor.YELLOW, 1, 3, false);
     // } else {
-    //   if (supersystemState.getElevatorStatus()) {
-    //     leds.breath("frontBottom", LedColor.GREEN, 2);
-    //     leds.breath("backBottom", LedColor.GREEN, 2);
-    //   } else {
-    //     leds.solid("frontBottom", LedColor.RED);
-    //     leds.solid("backBottom", LedColor.RED);
-    //   }
+    // if (supersystemState.getElevatorStatus()) {
+    // leds.breath("frontBottom", LedColor.GREEN, 2);
+    // leds.breath("backBottom", LedColor.GREEN, 2);
+    // } else {
+    // leds.solid("frontBottom", LedColor.RED);
+    // leds.solid("backBottom", LedColor.RED);
+    // }
     // }
   }
 
@@ -156,8 +183,65 @@ public class LedOperation extends SubsystemBase {
       case orange:
         color = LedColor.ORANGE;
         break;
-      default:
+      case gold:
+        color = LedColor.GOLD;
+        break;
+      case yellow:
+        color = LedColor.YELLOW;
+        break;
+      case yellowGreen:
+        color = LedColor.YELLOW_GREEN;
+        break;
+      case lime:
+        color = LedColor.LIME;
+        break;
+      case green:
+        color = LedColor.GREEN;
+        break;
+      case aqua:
         color = LedColor.AQUA;
+        break;
+      case seaBlue:
+        color = LedColor.SEA_BLUE;
+        break;
+      case lightBlue:
+        color = LedColor.LIGHT_BLUE;
+        break;
+      case skyBlue:
+        color = LedColor.SKY_BLUE;
+        break;
+      case blue:
+        color = LedColor.BLUE;
+        break;
+      case cornflower:
+        color = LedColor.CORNFLOWER;
+        break;
+      case indigo:
+        color = LedColor.INDIGO;
+        break;
+      case lightPurple:
+        color = LedColor.LIGHT_PURPLE;
+        break;
+      case purple:
+        color = LedColor.PURPLE;
+        break;
+      case lightPink:
+        color = LedColor.LIGHT_PINK;
+        break;
+      case rose:
+        color = LedColor.ROSE;
+        break;
+      case magenta:
+        color = LedColor.MAGENTA;
+        break;
+      case brown:
+        color = LedColor.BROWN;
+        break;
+      case white:
+        color = LedColor.WHITE;
+        break;
+      default:
+        color = LedColor.LIGHT_ORANGE;
     }
 
     switch (manualLedState) {
@@ -165,31 +249,34 @@ public class LedOperation extends SubsystemBase {
         leds.solid("full", color);
         break;
       case setTwoToneSolid:
-        leds.solidTwoColor("full", LedColor.ORANGE, LedColor.MAGENTA);
+        leds.solidTwoColor("full", LedColor.TURQUOISE, LedColor.PEACH);
         break;
-      case waveBlueGreen:
+      case fadeBlueGreen:
         leds.fade("full", LedColor.GREEN, LedColor.BLUE, 1, 3);
         break;
-      case breathLightBlue:
-        leds.breath("full", LedColor.LIGHT_BLUE, 3);
+      case breathColor:
+        leds.breath("full", color, 3);
         break;
       case rainbow:
         leds.rainbow("full", 3);
         break;
-      case strobeRed:
-        leds.strobe("full", LedColor.RED, 1);
+      case strobeColor:
+        leds.strobe("full", color, 1);
+        break;
+      case carnivalEasterGreenPurple:
+        leds.carnival("full", LedColor.EASTER_GREEN, LedColor.EASTER_PURPLE, 2, 4);
         break;
       case setColorBlack:
         leds.solid("full", LedColor.BLACK);
         break;
-      case fillGreen:
-        leds.fill("full", LedColor.GREEN, 1, 2, true);
+      case fillColor:
+        leds.fill("full", color, 1, 2, true);
         break;
-      case zipAqua:
-        leds.zip("full", LedColor.AQUA, 10, 1, 2, true);
+      case zipColor:
+        leds.zip("full", color, 10, 1, 2, true);
         break;
-      case wavePurple:
-        leds.wave("full", LedColor.BRIGHT_PURPLE, 3);
+      case waveColor:
+        leds.wave("full", color, 3);
       case colorTest:
         leds.colorTest("full", hueValue.getDouble(0));
         break;
